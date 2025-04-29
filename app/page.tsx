c1783a50eb3cba5ac3d5ce7bc61a1b15aed1d657
timur-urcan <urcantimur@gmail.com>
2025-04-29 18:26:35 +0200

    refactor: simplify page structure
    - Move page component to app/page.tsx
    - Remove language-related code and [lang] directory
    - Clean up component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
@@ -14,47 +12,36 @@ import Footer from '../components/Footer';
 import AnimateOnScroll from '../components/AnimateOnScroll';
 import ChatbotSimulation from '../components/ChatbotSimulation';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
       <Navbar />
       <HeroSection />
-      
       <AnimateOnScroll animation="fade-up">
         <AboutSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <AssetManagementSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <TestimonialsSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <ServicesSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <CaseStudiesSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <NewsInsightsSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <NewsletterCTA />
       </AnimateOnScroll>
-      
       <AnimateOnScroll animation="fade-up">
         <ContactSection />
       </AnimateOnScroll>
-      
       <Footer />
-      
-      {/* Chatbot simulation */}
       <ChatbotSimulation />
     </main>
   );
